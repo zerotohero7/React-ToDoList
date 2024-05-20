ee46e73159361ca15bd7e449212c22e983125b6a
Mohamed Nkaies <mohamednkaies3@gmail.com>
2024-05-20 15:00:21 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import { TodoList } from "./components/ToDoList";
+
+
+function App() {
+
+ 
+
+
+   
+  
+  return (
+    <div>
+
+      <TodoList/>
+
+     </div>
+    
+  );
+}
+
+ 
+
+export default App;
